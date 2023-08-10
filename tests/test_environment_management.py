038278f729b6f7d73fc9d81de08a329ec476c512
Kim Pevey <kcpevey@quansight.com>
2023-08-10 10:45:21 -0500

    fix tests

--- tests/test_environment_management.py
+++ tests/test_environment_management.py
@@ -1,11 +1,15 @@
+import pytest
+
 from argo_assistant import environment_management as aem
 
 
+@pytest.mark.nebari_resources
 def test_setup_argo_environment_verbose_true():
     # setup argo and hera environment variables
     aem.setup_argo_environment(verbose=True)
 
 
+@pytest.mark.nebari_resources
 def test_setup_argo_environment_verbose_false():
     # setup argo and hera environment variables
     aem.setup_argo_environment(verbose=False)
